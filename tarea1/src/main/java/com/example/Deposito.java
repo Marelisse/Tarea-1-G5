1685590be218c9d11526e3b6e1be82e343215363
Arcisternas <arcisternas2023@udec.cl>
2024-04-26 08:48:57 -0400

    Avance en clases "Moneda" y "Deposito"
    -Se definió la clase Deposito (De tipo genérico).
    
    -Se definió la clase Moneda, implementando la interfaz Comparable y el método compareTo.

--- tarea1/src/main/java/com/example/Deposito.java
+++ tarea1/src/main/java/com/example/Deposito.java
@@ -1,2 +1,18 @@
 package tarea1.src.main.java.com.example;
+class Deposito<T> {
+    private ArrayList<T> almacen;
 
+    public Deposito() {
+        almacen = new ArrayList<T>();
+    }
+
+    public void add(T t) {
+        almacen.add(t);
+    }
+    public T get() {
+        if(almacen.size() != 0) {
+            return almacen.remove(0);
+        }
+        return null;
+    }
+}
--- tarea1/src/main/java/com/example/Moneda.java
+++ tarea1/src/main/java/com/example/Moneda.java
@@ -1,5 +1,90 @@
 package tarea1.src.main.java.com.example;
 
-public class Moneda {
-    
+abstract class Moneda {
+    public Moneda(){
+
+    }
+    public Moneda getSerie(){
+        return this;
+    }
+    public abstract int getValor();
+}
+class Moneda1500 extends Moneda{
+    public Moneda1500(){
+        super();
+    }
+    public int getValor(){
+        return 1500;
+    }
+
+    @Override
+    public int compareTo(Moneda o) {
+        if (this.getValor() > o.getValor()) {
+            return 1;
+        } else if (this.getValor() < o.getValor()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
+class Moneda1000 extends Moneda {
+    public Moneda1000() {
+        super();
+    }
+
+    public int getValor() {
+        return 1000;
+    }
+
+    @Override
+    public int compareTo(Moneda o) {
+        if (this.getValor() > o.getValor()) {
+            return 1;
+        } else if (this.getValor() < o.getValor()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
+class Moneda500 extends Moneda{
+    public Moneda500(){
+        super();
+    }
+    public int getValor(){
+        return 500;
+    }
+    @Override
+    public int compareTo(Moneda o) {
+        if (this.getValor() > o.getValor()) {
+            return 1;
+        } else if (this.getValor() < o.getValor()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
+class Moneda100 extends Moneda{
+    public Moneda100(){
+        super();
+    }
+    public int getValor(){
+        return 100;
+    }
+    @Override
+    public int compareTo(Moneda o) {
+        if (this.getValor() > o.getValor()) {
+            return 1;
+        } else if (this.getValor() < o.getValor()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
