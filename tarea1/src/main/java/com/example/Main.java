34055f893c3153bb21524eaffaef7c73b8e52fb4
María Jesús <marolivares2023@udec.cl>
2024-04-26 19:16:03 -0400

    feat: Main
    -Se agregan funcionalidades al main para poder probar el programa utilizando try y catch

--- tarea1/src/main/java/com/example/Main.java
+++ tarea1/src/main/java/com/example/Main.java
@@ -1,1 +1,37 @@
 package com.example;
+
+import com.example.Exeptions.NoHayProductoException;
+import com.example.Exeptions.PagoIncorrectoException;
+import com.example.Exeptions.PagoInsuficienteException;
+
+public class Main {
+    public static void main(String[] args) {
+        Expendedor exp = new Expendedor(6);
+        Moneda m = null;
+        Comprador c = null;
+
+        m = new Moneda1500();
+        try {
+            c = new Comprador(m, 1, exp);
+        } catch (PagoInsuficienteException | NoHayProductoException | PagoIncorrectoException e) {
+            e.printStackTrace();
+        }
+        System.out.println(c.queConsumiste() + ", "+ c.cuantoVuelto());
+
+        m = new Moneda1500();
+        try {
+            c = new Comprador(m, 2, exp);
+        } catch (PagoInsuficienteException | NoHayProductoException | PagoIncorrectoException e) {
+            e.printStackTrace();
+        }
+        System.out.println(c.queConsumiste() + ", "+ c.cuantoVuelto());
+
+        m = new Moneda1500();
+        try {
+            c = new Comprador(m, 3, exp);
+        } catch (PagoInsuficienteException | NoHayProductoException | PagoIncorrectoException e) {
+            e.printStackTrace();
+        }
+        System.out.println(c.queConsumiste() + ", "+ c.cuantoVuelto());
+    }
+}
