d0be52ea8644582e267bf1b7a29a8932b6c6aefe
María Jesús <marolivares2023@udec.cl>
2024-04-28 12:38:26 -0400

    fix: Comprador
    - Se realiza cambio de ciclo para obtener una correcta devolución del vuelto

--- tarea1/src/main/java/com/example/Comprador.java
+++ tarea1/src/main/java/com/example/Comprador.java
@@ -8,18 +8,18 @@ class Comprador{
     private int vuelto;
     private String sonido;
     public Comprador(Moneda m, int queproducto, Expendedor exp) throws PagoInsuficienteException, NoHayProductoException, PagoIncorrectoException{
-        Moneda n = exp.getVuelto();
         Producto p = exp.comprarProducto(m, queproducto);
-        this.vuelto = 0;
 
-        while(n!=null){
-            this.vuelto += n.getValor();
-            n = exp.getVuelto();
-        }
         if(p==null){
             sonido = null;
         }else{
             sonido = p.consumir();
+        }
+
+        vuelto = 0;
+
+        for(int i = 0; i <exp.getVuelto(); i++){
+            vuelto += 100;
         }
     }
     public int cuantoVuelto(){
