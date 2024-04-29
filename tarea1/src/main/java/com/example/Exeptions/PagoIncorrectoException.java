fa8e7d9c45555d2a70e8ea514486af402a7f2b9c
María Jesús <marolivares2023@udec.cl>
2024-04-29 00:39:49 -0400

    docs: Actualización de la documentación de la clase PagoIncorrectoException

--- tarea1/src/main/java/com/example/Exeptions/PagoIncorrectoException.java
+++ tarea1/src/main/java/com/example/Exeptions/PagoIncorrectoException.java
@@ -1,6 +1,12 @@
 package com.example.Exeptions;
-
+/**
+ * Excepcion cuando no se ingresa una Moneda (null)
+ * @author Ariel Cisternas
+ */
 public class PagoIncorrectoException extends Exception {
+    /**
+     * @param errorMessage Se ingresa un String con el mensaje de error
+     */
     public PagoIncorrectoException(String errorMessage){
         super(errorMessage);
     }
