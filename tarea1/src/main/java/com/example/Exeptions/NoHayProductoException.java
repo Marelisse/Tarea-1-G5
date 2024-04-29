ab42e1071816c6bbc49785c7b276ea9cd00d44fc
María Jesús <marolivares2023@udec.cl>
2024-04-29 00:39:33 -0400

    docs: Actualización de la documentación de la clase NoHayProductoException

--- tarea1/src/main/java/com/example/Exeptions/NoHayProductoException.java
+++ tarea1/src/main/java/com/example/Exeptions/NoHayProductoException.java
@@ -1,6 +1,13 @@
 package com.example.Exeptions;
-
+/**
+ * Excepcion cuando no hay producto o el Deposito es Erroneo
+ * @author Ariel Cisternas
+ */
 public class NoHayProductoException extends Exception {
+    
+    /**
+     * @param errorMessage Se ingresa un String con el mensaje de error
+     */
     public NoHayProductoException(String errorMessage){
         super(errorMessage);
     }
