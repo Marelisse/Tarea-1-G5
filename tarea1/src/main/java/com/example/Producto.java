c10e5220fe528d203a00813c8840d1fd31d028e6
María Jesús <marolivares2023@udec.cl>
2024-04-29 00:41:13 -0400

    docs: Actualización de la documentación de la clase Producto

--- tarea1/src/main/java/com/example/Producto.java
+++ tarea1/src/main/java/com/example/Producto.java
@@ -1,12 +1,33 @@
 package com.example;
 
+/**
+ * SuperClase de Productos de tipo Bebida y Dulce
+ * @author Maria Jesus Olivares
+ */
+
 abstract class Producto {
+    //int para almacenar el numero de serie de los productos
     private int serie;
+    /**
+     * Constructor donde se registrará el numero de serie
+     * @param serie este parametro define el numero de serie de los productos
+     */
     public Producto(int serie) {
         this.serie = serie;
-    }
+    } 
+    /**
+     * Metodo en el cual se utiliza un getter para obtener el numero de serie
+     * @return retorna el nuemero de serie guardado en el int
+     */
     public int getSerie(){
         return serie;
     }
+    /**
+     * Metodo abstracto que utilizarán y definirán las subclases de Bebida y Dulce
+     * @return un String con el nombre del producto
+     * @see Dulce
+     * @see Bebida
+     */
     public abstract String consumir();
-}
+    
+} 
