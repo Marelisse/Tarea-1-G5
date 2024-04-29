cdc1b6d30fd4e85cbd208190313a5744691a33e5
María Jesús <marolivares2023@udec.cl>
2024-04-29 00:40:37 -0400

    docs: Actualización de la documentación del enum LosProductos

--- tarea1/src/main/java/com/example/LosProductos.java
+++ tarea1/src/main/java/com/example/LosProductos.java
@@ -1,19 +1,36 @@
 package com.example;
 
+/**
+ * Enumeración de los Productos
+ * @author Maria Jesus Olivares
+ */
+
 enum LosProductos{
+    /** Se enumeran los productos (Sean Dulces o Bebidas) para facilitar procesos */ 
     SNICKERS(1),
     SUPER8(2),
     COCACOLA(3),
     SPRITE(4),
     FANTA(5);
 
+    /** Int para asignar precios a cada producto*/
     private int precio;
+    /** Final int del numero de cada producto, así no se cambia su enumeracion*/
     private final int num;
 
+    /**
+     * "Constructor" que proporciona el numero de cada producto
+     * @param num Int final que corresponde a cada producto (Snickers es 1)
+     */
     LosProductos(int num){
         this.num = num;
     }
 
+    /**
+     * Metodo que asigna y retorna un precio fijado de cada producto
+     * Se utiliza switch / case según el numero del cual se consulta (Snickers(1) = 1100)
+     * @return int del Precio del Producto seleccionado
+     */
     public int getPrecio(){
         switch (num) {
             case 1:
