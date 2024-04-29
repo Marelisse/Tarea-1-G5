62e8f40e43bae6cbf07dad09c6b5560b307cfd96
María Jesús <marolivares2023@udec.cl>
2024-04-29 00:40:03 -0400

    docs: Actualización de la documentación de la clase PagoInsuficienteException

--- tarea1/src/main/java/com/example/Exeptions/PagoInsuficienteException.java
+++ tarea1/src/main/java/com/example/Exeptions/PagoInsuficienteException.java
@@ -1,6 +1,14 @@
 package com.example.Exeptions;
 
+/**
+ * Excepcion cuando la Moneda es menor al Precio
+ * @author Ariel Cisternas
+ */
+
 public class PagoInsuficienteException extends Exception {
+    /**
+     * @param errorMessage Se ingresa un String con el mensaje de error 
+     */
     public PagoInsuficienteException(String errorMessage){
         super(errorMessage);
     }
