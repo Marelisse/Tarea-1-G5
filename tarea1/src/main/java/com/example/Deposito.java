f80489b0f007278fa54d3542a02c36872fbeae82
María Jesús <marolivares2023@udec.cl>
2024-04-29 00:38:50 -0400

    docs: Actualización de la documentación de la clase Deposito

--- tarea1/src/main/java/com/example/Deposito.java
+++ tarea1/src/main/java/com/example/Deposito.java
@@ -1,15 +1,29 @@
 package com.example;
 import java.util.ArrayList;
+/**
+ * Clase Deposito el cual se puede utilizar para guardar Monedas, Dulces y Bebidas
+ * @author Ariel Cisternas
+ */
 class Deposito<T> {
+    /**Lista donde se guardaran los productos/monedas segun el tipo*/
     private ArrayList<T> almacen;
 
+    /**Constructor que segun la clase, se crea una lista correspondiente de cada producto o moneda instanciada*/
     public Deposito() {
         almacen = new ArrayList<T>();
     }
 
+    /**
+     * Metodo donde se añaden los productos al almacen
+     * @param t Serie de los productos o Moneda a guardar en el almacen
+     */
     public void add(T t) {
         almacen.add(t);
     }
+    /**
+     * Metodo para obtener un producto/moneda
+     * @return el producto/moneda o null si no existen mas productos de la misma categoria
+     */
     public T get() {
         if(almacen.size() != 0) {
             return almacen.remove(0);
