9846c2b2cdc70421a1b8def17a0fa87c2ebf7379
María Jesús <marolivares2023@udec.cl>
2024-04-29 00:38:57 -0400

    docs: Actualización de la documentación de la clase Dulce

--- tarea1/src/main/java/com/example/Dulce.java
+++ tarea1/src/main/java/com/example/Dulce.java
@@ -1,18 +1,42 @@
 package com.example;
 
+/**
+ * Producto de tipo Dulce
+ * @author Maria Jesus Olivares
+ */
+
 abstract class Dulce extends Producto{
+    /**
+     * Constructor para el Producto de clase Dulce (Subclase de Producto)
+     * @see Producto 
+     * @param serie este parametro define el numero de serie de cada Dulce
+     */
     public Dulce(int serie){
         super(serie);
-    }
-}
+    } 
+} 
+
+/**
+ * Las siguientes clases son Subclases de la clase abstracta Dulce (superclase de estas)
+ * Todas comparten características y métodos, por lo que se tomará de ejemplo solo una de estas
+ */
 class Snickers extends Dulce{
+    /**
+     * Constructor del Dulce Snickers
+     * @param serie asigna el valor de numero de serie invocando a la superclase
+     */
     public Snickers(int serie){
         super(serie);
     }
+    /**
+     * Metodo que devuelve el nombre de la Bebida al Consumirse
+     * @return Un String con el nombre, en este caso "snickers"
+     */
     public String consumir(){
         return "snickers";
-    }
+    } 
 }
+
 class Super8 extends Dulce{
     public Super8(int serie){
         super(serie);
